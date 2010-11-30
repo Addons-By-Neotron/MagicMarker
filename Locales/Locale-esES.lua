7654ad41f0bc82d25d7dada05121dbca1101c427
David Hedbor <neotron@gmail.com>
2010-11-30 06:43:40 +0000

    Moved to use WowAce localization.

--- Locales/Locale-esES.lua
+++ Locales/Locale-esES.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("MagicMarker", "zhTW")
+local L = LibStub("AceLocale-3.0"):NewLocale("MagicMarker", "esES")
 if not L then return end
 
 -- To help localize MagicMarker please enter phrase translations on the
@@ -6,4 +6,4 @@ if not L then return end
 -- http://www.wowace.com/projects/magic-marker/localization/
 -- This file should not be edited manually!
 
---@localization(locale="zhTW", format="lua_additive_table", handle-unlocalized="english")@
+--@localization(locale="esES", format="lua_additive_table", handle-unlocalized="english")@
